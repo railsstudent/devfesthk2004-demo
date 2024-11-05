6710ab0900329afacd0a4b9076b98cfaf9f49c52
Connie Leung <cleung334@gmail.com>
2024-11-05 23:13:35 +0800

    refactor: move common Prompt API to base service

--- projects/prompt-api-demo/src/app/ai/services/system-prompts.service.ts
+++ projects/prompt-api-demo/src/app/ai/services/system-prompts.service.ts
@@ -3,79 +3,18 @@ import { from } from 'rxjs';
 import { AI_PROMPT_API_TOKEN } from '../constants/core.constant';
 import { LanguageModelCapabilities } from '../types/language-model-capabilties.type';
 import { Tokenization } from '../types/prompt.type';
+import { AbstractPromptService } from './abstract-prompt.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SystemPromptService {
-  #promptApi = inject(AI_PROMPT_API_TOKEN);
-  #session = signal<any | null>(null);
-  session = this.#session.asReadonly();
+export class SystemPromptService extends AbstractPromptService {
   #controller = new AbortController();
-  #tokenContext = signal<Tokenization | null>(null);
-  tokenContext = this.#tokenContext.asReadonly();
-
-  private resetSession(newSession: any) {
-    this.#session.set(newSession);
-    this.#tokenContext.set(null);
-  }
 
   async createSession(systemPrompt: string) {
-    const oldSession = this.#session();
-    if (oldSession) {
-      console.log('Destroy the prompt session.');
-      oldSession.destroy();
-    }
+    this.destroySession();
     
-    const newSession = await this.#promptApi?.create({ systemPrompt }, { signal: this.#controller.signal });
+    const newSession = await this.promptApi?.create({ systemPrompt }, { signal: this.#controller.signal });
     this.resetSession(newSession);
-  }
-
-  getCapabilities() {
-    if (!this.#promptApi) {
-      throw new Error(`Your browser doesn't support the Prompt API. If you are on Chrome, join the Early Preview Program to enable it.`);
-    } else if (!this.#promptApi.capabilities) {
-      throw new Error('Capabilities detection is unsupported. Please check your configuration in chrome://flags/#optimization-guide-on-device-model');
-    }
-
-    return from(this.#promptApi.capabilities() as Promise<LanguageModelCapabilities>);
-  }
-
-  async prompt(query: string): Promise<string> {
-    if (!this.#promptApi) {
-      throw new Error(`Your browser doesn't support the Prompt API. If you are on Chrome, join the Early Preview Program to enable it.`);
-    }
-
-    const session = this.session();
-    if (!session) {
-      throw new Error('Failed to create AITextSession.');
-    }
-
-    const answer = await session.prompt(query);
-    this.#tokenContext.set({
-      tokensSoFar: session.tokensSoFar as number,
-      maxTokens: session.maxTokens as number,
-      tokensLeft: session.tokensLeft as number,
-    })
-
-    return answer;
-  }
-
-  countNumTokens(query: string): Promise<number> {
-    if (!this.#session) {
-      return Promise.resolve(0);
-    }
-
-    const session = this.#session();
-    return session.countPromptTokens(query) as Promise<number>;
-  }
-
-  destroySession() {
-    const session = this.session();
-
-    if (session) {
-      session.destroy();
-      this.resetSession(null);
-    }
   }
 }
--- projects/prompt-api-demo/src/app/ai/services/zero-prompt.service.ts
+++ projects/prompt-api-demo/src/app/ai/services/zero-prompt.service.ts
@@ -1,33 +1,19 @@
-import { inject, Injectable, signal } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { firstValueFrom, from } from 'rxjs';
-import { AI_PROMPT_API_TOKEN } from '../constants/core.constant';
 import { LanguageModelCapabilities } from '../types/language-model-capabilties.type';
-import { SessionConfiguration, Tokenization } from '../types/prompt.type';
+import { SessionConfiguration } from '../types/prompt.type';
+import { AbstractPromptService } from './abstract-prompt.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ZeroPromptService {
-  #promptApi = inject(AI_PROMPT_API_TOKEN);
-  #session = signal<any | null>(null);
-  session = this.#session.asReadonly();
+export class ZeroPromptService extends AbstractPromptService  {
   #controller = new AbortController();
-  #tokenContext = signal<Tokenization | null>(null);
-  tokenContext = this.#tokenContext.asReadonly();
   #perSession = signal<{ topK: number, temperature: number } | undefined>(undefined);
   perSession = this.#perSession.asReadonly();
 
-  private resetSession(newSession: any) {
-    this.#session.set(newSession);
-    this.#tokenContext.set(null);
-  }
-
   async createSession(isPerSession: boolean, configuration: SessionConfiguration) {
-    const oldSession = this.#session();
-    if (oldSession) {
-      console.log('Destroy the prompt session.');
-      oldSession.destroy();
-    }
+    this.destroySession();
     
     const capabilities = await firstValueFrom(this.getCapabilities());
     const temperature = Math.min(configuration.temperature, 3);
@@ -44,55 +30,17 @@ export class ZeroPromptService {
     });
 
     console.log('createOptions', createOptions);
-    const newSession = await this.#promptApi?.create(createOptions);
+    const newSession = await this.promptApi?.create(createOptions);
     this.resetSession(newSession);
   }
 
   getCapabilities() {
-    if (!this.#promptApi) {
+    if (!this.promptApi) {
       throw new Error(`Your browser doesn't support the Prompt API. If you are on Chrome, join the Early Preview Program to enable it.`);
-    } else if (!this.#promptApi.capabilities) {
+    } else if (!this.promptApi.capabilities) {
       throw new Error('Capabilities detection is unsupported. Please check your configuration in chrome://flags/#optimization-guide-on-device-model');
     }
 
-    return from(this.#promptApi.capabilities() as Promise<LanguageModelCapabilities>);
-  }
-
-  async prompt(query: string): Promise<string> {
-    if (!this.#promptApi) {
-      throw new Error(`Your browser doesn't support the Prompt API. If you are on Chrome, join the Early Preview Program to enable it.`);
-    }
-
-    const session = this.session();
-    if (!session) {
-      throw new Error('Failed to create AITextSession.');
-    }
-
-    const answer = await session.prompt(query);
-    this.#tokenContext.set({
-      tokensSoFar: session.tokensSoFar as number,
-      maxTokens: session.maxTokens as number,
-      tokensLeft: session.tokensLeft as number,
-    })
-
-    return answer;
-  }
-
-  countNumTokens(query: string): Promise<number> {
-    if (!this.#session) {
-      return Promise.resolve(0);
-    }
-
-    const session = this.#session();
-    return session.countPromptTokens(query) as Promise<number>;
-  }
-
-  destroySession() {
-    const session = this.session();
-
-    if (session) {
-      session.destroy();
-      this.resetSession(null);
-    }
+    return from(this.promptApi.capabilities() as Promise<LanguageModelCapabilities>);
   }
 }
