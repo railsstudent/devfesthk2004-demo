6309351039993a180f276d807db39858729e63fd
Connie Leung <cleung334@gmail.com>
2024-11-07 09:52:33 +0800

    refactor:  split into LanguageAvailableComponent and LanguageDetectionResultComponent.

--- projects/language-detector-demo/src/app/language-detection/language-detection.component.ts
+++ projects/language-detector-demo/src/app/language-detection/language-detection.component.ts
@@ -3,19 +3,17 @@ import { FormsModule } from '@angular/forms';
 import { LanguageDetectionService } from '../ai/services/language-detection.service';
 import { LanguageAvailable } from '../ai/types/language-available.type';
 import { LanguageDetectionWithNameResult } from '../ai/types/language-detection-result.type';
+import { LanguageAvailabledComponent } from './components/language-available.component';
+import { LanguageDetectionResultComponent } from './components/language-detection-result.component';
 
 @Component({
   selector: 'app-language-detection',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, LanguageAvailabledComponent, LanguageDetectionResultComponent],
   template: `
     <div style="border: 1px solid black; border-radius: 0.25rem; padding: 1rem;">
       <h3>Language Detection Demo</h3>
-      <div>
-        @for (language of languagesAvailable(); track language.code) {
-          <p>Is {{ language.name }} available? {{language.available }}</p>
-        }
-      </div>
+      <app-language-available [languagesAvailable]="languagesAvailable()" />
       <div>
         <span class="label" for="input">Input text: </span>
         <textarea id="input" name="input" [(ngModel)]="inputText" rows="3"></textarea>
@@ -23,16 +21,7 @@ import { LanguageDetectionWithNameResult } from '../ai/types/language-detection-
       <button style="margin-right: 0.5rem;" (click)="setup()">Create capabilities and detector</button>
       <button style="margin-right: 0.5rem;" (click)="teardown()">Destroy capabilities and detector</button>
       <button (click)="detectLanguage()" [disabled]="isDisableDetectLanguage()">Detect Language</button>
-      <div>
-        <span class="label">Response: </span>
-        @for (language of detectedLanguages(); track language.detectedLanguage) {
-          <p>
-            <span>Confidence: {{ language.confidence.toFixed(3) }}, </span>
-            <span>Detected Language: {{ language.detectedLanguage }}, </span>
-            <span>Detected Language Name: {{ language.name }}</span>
-          </p>
-        }
-      </div>
+      <app-language-detection-result [detectedLanguages]="detectedLanguages()" />
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -58,7 +47,7 @@ export class LanguageDetectionComponent {
     await this.service.createDetector();
     const languages = this.languagesAvailable().map(({ code }) => ({
       code,
-      name: this.service.languageTagToHumanReadable(code) || 'NA',
+      name: this.service.languageTagToHumanReadable(code),
       available: this.capabilities().languageAvailable(code),
     }));
     this.languagesAvailable.set(languages);
@@ -69,7 +58,7 @@ export class LanguageDetectionComponent {
     await this.service.destroyDetector();
     const languages = this.languagesAvailable().map(({ code }) => ({
       code,
-      name: this.service.languageTagToHumanReadable(code) || 'NA',
+      name: this.service.languageTagToHumanReadable(code),
       available: 'no',
     }));
     this.languagesAvailable.set(languages);
