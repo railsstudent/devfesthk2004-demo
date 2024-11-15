46c3f8385925c763e123c6abae1610ece10ef220
Connie Leung <cleung334@gmail.com>
2024-11-15 08:30:11 +0800

    chore:  extract to type

--- projects/prompt-api-demo/src/app/ai/constants/core.constant.ts
+++ projects/prompt-api-demo/src/app/ai/constants/core.constant.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from '@angular/core';
 
-export const AI_PROMPT_API_TOKEN = new InjectionToken<{ create: Function, capabilities: Function } | undefined>('AI_PROMPT_API_TOKEN');
+type PromptAPIDefinitions = { create: Function, capabilities: Function } | undefined;
+export const AI_PROMPT_API_TOKEN = new InjectionToken<PromptAPIDefinitions>('AI_PROMPT_API_TOKEN');
