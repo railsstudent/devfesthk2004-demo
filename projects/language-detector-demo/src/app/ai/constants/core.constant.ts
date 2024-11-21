83b0404ed550b4c360fe19faf76e43e6a6ebcf45
Connie Leung <cleung334@gmail.com>
2024-11-21 23:22:13 +0800

    refactor:  type LanguageDetector

--- projects/language-detector-demo/src/app/ai/constants/core.constant.ts
+++ projects/language-detector-demo/src/app/ai/constants/core.constant.ts
@@ -1,12 +1,18 @@
 import { InjectionToken } from '@angular/core';
+import { LanguageDetectionResult } from '../types/language-detection-result.type';
 
 export type AILanguageDetectorCapabilities = {
     available: AICapabilityAvailability;
     languageAvailable: (code: string) => AICapabilityAvailability;
 }
 
+export type AILanguageDetector = {
+    destroy: () => void;
+    detect: (query: string) => Promise<LanguageDetectionResult[]>;
+}
+
 type LANGUAGE_DETECTOR_API_TYPE = { 
-    create: Function, 
+    create: (...args: unknown[]) => Promise<AILanguageDetector>, 
     capabilities: (...args: unknown[]) => Promise<AILanguageDetectorCapabilities> 
 };
 export const AI_LANGUAGE_DETECTION_API_TOKEN = new InjectionToken<LANGUAGE_DETECTOR_API_TYPE | undefined>('AI_LANGUAGE_DETECTION_API_TOKEN');
--- projects/language-detector-demo/src/app/ai/services/language-detection.service.ts
+++ projects/language-detector-demo/src/app/ai/services/language-detection.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { AI_LANGUAGE_DETECTION_API_TOKEN, AILanguageDetectorCapabilities } from '../constants/core.constant';
+import { AI_LANGUAGE_DETECTION_API_TOKEN, AILanguageDetector, AILanguageDetectorCapabilities } from '../constants/core.constant';
 import { LanguageDetectionResult, LanguageDetectionWithNameResult } from '../types/language-detection-result.type';
 
 const MAX_LANGUAGE_RESULTS = 111;
@@ -11,7 +11,7 @@ export class LanguageDetectionService  {
     #controller = new AbortController();
 
     #languageDetectionAPI = inject(AI_LANGUAGE_DETECTION_API_TOKEN);
-    #detector = signal<any | null>(null);
+    #detector = signal<AILanguageDetector| undefined>(undefined);
     detector = this.#detector.asReadonly();
     #capabilities = signal<AILanguageDetectorCapabilities | null>(null);
     capabilities = this.#capabilities.asReadonly();
@@ -35,11 +35,11 @@ export class LanguageDetectionService  {
 
         const detector = this.detector();
         if (!detector) {
-            throw new Error('Failed to create LanguageDetector.');
+            throw new Error('Failed to create the LanguageDetector.');
         }
 
         const minTopNReesults = Math.min(topNResults, MAX_LANGUAGE_RESULTS);
-        const results = (await detector.detect(query)) as LanguageDetectionResult[];
+        const results = await detector.detect(query);
         const probablyLanguages = results.slice(0, minTopNReesults);
         return probablyLanguages.map((item) => ({ ...item, name: this.languageTagToHumanReadable(item.detectedLanguage) }))
     }
@@ -50,18 +50,18 @@ export class LanguageDetectionService  {
         if (detector) {
             detector.destroy();
             console.log('Destroy the language detector.');
-            this.#detector.set(null);
+            this.#detector.set(undefined);
         }
     }
 
-  async createDetector() {
-    this.destroyDetector();
-    const newDetector = await this.#languageDetectionAPI?.create({ signal: this.#controller.signal });
-    this.#detector.set(newDetector);
-  }
+    async createDetector() {
+        this.destroyDetector();
+        const newDetector = await this.#languageDetectionAPI?.create({ signal: this.#controller.signal });
+        this.#detector.set(newDetector);
+    }
 
-  languageTagToHumanReadable(languageTag: string, targetLanguage = 'en') {
-    const displayNames = new Intl.DisplayNames([targetLanguage], { type: 'language' });
-    return displayNames.of(languageTag) || 'NA';
-  }
+    languageTagToHumanReadable(languageTag: string, targetLanguage = 'en') {
+        const displayNames = new Intl.DisplayNames([targetLanguage], { type: 'language' });
+        return displayNames.of(languageTag) || 'NA';
+    }
 }
