9a83ed2850a4c3b7663923e8ccd0e8887f3679d4
Connie Leung <cleung334@gmail.com>
2024-11-27 09:16:06 +0800

    refactor:  upgrade the typed definition

--- projects/prompt-api-demo/src/app/ai/types/prompt.type.ts
+++ projects/prompt-api-demo/src/app/ai/types/prompt.type.ts
@@ -1,5 +1,5 @@
 export type Tokenization = Pick<AILanguageModel, "tokensSoFar" | "tokensLeft" | "maxTokens">;
 
 export type LanguageInitialPrompt = 
-    [AILanguageModelSystemPrompt, ...Array<AILanguageModelAssistantPrompt | AILanguageModelUserPrompt>]
-    | Array<AILanguageModelAssistantPrompt | AILanguageModelUserPrompt>;
+    [AILanguageModelSystemPrompt, ...AILanguageModelPrompt[]]
+    | AILanguageModelPrompt[];
