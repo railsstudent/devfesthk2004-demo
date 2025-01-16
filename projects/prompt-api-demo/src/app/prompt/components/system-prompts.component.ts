3bd34eff32f92f69a03e1e8ae437a03a68ecf42a
Connie Leung <cleung334@gmail.com>
2025-01-16 08:28:07 +0800

    fix: the system prompt based on Grammerly's suggestion

--- projects/prompt-api-demo/src/app/prompt/components/system-prompts.component.ts
+++ projects/prompt-api-demo/src/app/prompt/components/system-prompts.component.ts
@@ -33,7 +33,7 @@ import { PromptResponseComponent } from './prompt-response.component';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SystemPromptsComponent extends BasePromptComponent {
-  systemPrompt = signal(`You are a professional trip planner who helps travelers to plan a trip to a location. When a traveler specifies a country or a city, you have to recommend how to apply travel visa, pack suitable clothes for the weather and essentials, and the known attractions to visit each day. It is preferred to visit two to three attractions each day to maximize the value of the trip. If you don't know the answer, say "I do not know the answer."`);
+  systemPrompt = signal(`You are a professional trip planner who helps travelers to plan a trip to a location. When a traveler specifies a country or city, you have to recommend how to apply for travel visas, pack suitable clothes for the weather and essentials, and list the known attractions to visit daily. It is preferred to visit two to three attractions each day to maximize the value of the trip. If you don't know the answer, say, "I do not know the answer."`);
   tokenContext = this.promptService.tokenContext;
   
   responseState = computed<PromptResponse>(() => ({
@@ -46,7 +46,7 @@ export class SystemPromptsComponent extends BasePromptComponent {
 
   constructor() {
     super();
-    this.query.set('I will visit from Hong Kong to Taipei between Feb 13th to Feb 18th. Please help me plan the trip and assume I will arrive in the afternoon on day 1.');
+    this.query.set('I will visit from Hong Kong to Taipei between Feb 13th to Feb 18th. Please help me plan the trip and assume I will arrive in the afternoon of the 13th.');
     toObservable(this.systemPrompt) 
       .pipe(
         debounceTime(300),
