a6f91612a9a711da9a88d28246ae352f4568626f
Connie Leung <cleung334@gmail.com>
2025-05-30 00:36:09 +0800

    fix: use NO_GEMINI_NANO error code

--- projects/language-detector-demo/src/app/ai/utils/ai-detection.ts
+++ projects/language-detector-demo/src/app/ai/utils/ai-detection.ts
@@ -8,7 +8,7 @@ export async function getLanguageDetectorAPIAvailability(): Promise<string> {
 
    const availability = await LanguageDetector.availability();
    if (availability === 'unavailable') { 
-      throw new Error(ERROR_CODES.NO_API);
+      throw new Error(ERROR_CODES.NO_GEMINI_NANO);
    }
 
    return availability;
