3ce3506d6553a1f32bed217f66728936f038b515
Connie Leung <cleung334@gmail.com>
2025-06-01 14:38:32 +0800

    refactor:  log error and display it on UI

--- projects/translation-api-demo/src/app/ai/services/translator.service.ts
+++ projects/translation-api-demo/src/app/ai/services/translator.service.ts
@@ -45,6 +45,26 @@ export class TranslatorService  {
         }) 
     }
 
+    private handleErrors(e: unknown, languagePair: LanguagePair) {
+        if (e instanceof DOMException && e.name === 'InvalidStateError') {
+            console.error('The document is not active. Please try again later.');
+        } else if (e instanceof DOMException && e.name === 'NetworkError') {
+            console.error('The network is not available to download the AI model.');
+        } else if (e instanceof DOMException && e.name === 'NotAllowedError') {
+            console.error('The Translator is not allowed to create. Language pair:', languagePair);
+        } else if (e instanceof DOMException && e.name === 'NotSupportedError') {
+            console.error('The Translator does not support one of the languages. Language pair:', languagePair);
+        } else if (e instanceof DOMException && e.name === 'OperationError') {
+            console.error('Operation error occurred when creating the translator for the language pair:', languagePair);
+        } else if (e instanceof DOMException && e.name === 'QuotaExceededError') {
+            console.error('Translator API Quota exceeded. Please try again later.');
+        } else if (e instanceof Error) {
+            console.error(e.message);
+        } else {
+            console.error(e);
+        }
+    }
+
     async translate(languagePair: LanguagePair, inputText: string): Promise<string> {
         try { 
             const translator = await this.createTranslator(languagePair);
@@ -58,23 +78,7 @@ export class TranslatorService  {
 
             return translatedText;
         } catch (e) {
-            if (e instanceof DOMException && e.name === 'InvalidStateError') {
-                console.error('The document is not active. Please try again later.');
-            } else if (e instanceof DOMException && e.name === 'NetworkError') {
-                console.error('The network is not available to download the AI model.');
-            } else if (e instanceof DOMException && e.name === 'NotAllowedError') {
-                console.error('The Translator is not allowed to create. Language pair:', languagePair);
-            } else if (e instanceof DOMException && e.name === 'NotSupportedError') {
-                console.error('The Translator does not support one of the languages. Language pair:', languagePair);
-            } else if (e instanceof DOMException && e.name === 'OperationError') {
-                console.error('Operation error occurred when creating the translator for the language pair:', languagePair);
-            } else if (e instanceof DOMException && e.name === 'QuotaExceededError') {
-                console.error('Translator API Quota exceeded. Please try again later.');
-            } else if (e instanceof Error) {
-                console.error(e.message);
-            } else {
-                console.error(e);
-            }
+            this.handleErrors(e, languagePair);
             return '';
         }
     }
@@ -82,28 +86,16 @@ export class TranslatorService  {
     async downloadLanguagePackage(languagePair: LanguagePair): Promise<LanguagePairAvailable> {
         try { 
             const translator = await this.createTranslator(languagePair);
+            if (!translator) {
+                return { ...languagePair, available: 'unavailable'};
+            }
             translator.destroy();
 
-            const available = await Translator.availability(languagePair)
+            const available = await Translator.availability(languagePair);
             return { ...languagePair, available };
         } catch (e) {
-            if (e instanceof DOMException && e.name === 'InvalidStateError') {
-                console.error('The document is not active. Please try again later.');
-            } else if (e instanceof DOMException && e.name === 'NetworkError') {
-                console.error('The network is not available to download the model.');
-            } else if (e instanceof DOMException && e.name === 'NotAllowedError') {
-                console.error('Translator is not allowed to create. Language pair:', languagePair);
-            } else if (e instanceof DOMException && e.name === 'NotSupportedError') {
-                console.error('Translator does not support one of the languages. Language pair:', languagePair);
-            } else if (e instanceof DOMException && e.name === 'OperationError') {
-                console.error('Operation error occurred when creating a translator for language pair:', languagePair);
-            } else if (e instanceof Error) {
-                console.error(e.message);
-            } else {
-                console.error(e);
-            }
-
-            return { ...languagePair, available: 'unavailable'}
+            this.handleErrors(e, languagePair);
+            return { ...languagePair, available: 'unavailable'};
         }
     }
 
