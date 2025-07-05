6fa47aafe495cdbff9ca8121b15018f02c733de6
Connie Leung <cleung334@gmail.com>
2025-07-05 16:32:26 +0800

    chore: create .nojekyll file

--- deploy-github-page.sh
+++ deploy-github-page.sh
@@ -8,6 +8,7 @@ fi
 echo "delete dist/$1"
 rm -rf dist/$1
 echo "build project $1 starts"
-ng build --project=$1 --output-path dist/$1
+ng build --project=$1
 cp ./dist/$1/index.html  ./dist/$1/404.html
+touch "./dist/$1/.nojekyll"
 echo 'build project finishes'
