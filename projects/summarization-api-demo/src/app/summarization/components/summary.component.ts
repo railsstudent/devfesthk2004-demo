09f5d0cc774adaac2cbbe08d1a4d9c024bce9a5c
Connie Leung <cleung334@gmail.com>
2025-07-17 22:38:23 +0800

    refactor: avoid async/await hell when streaming summary

--- projects/summarization-api-demo/src/app/summarization/components/summary.component.ts
+++ projects/summarization-api-demo/src/app/summarization/components/summary.component.ts
@@ -1,4 +1,4 @@
-import { afterRenderEffect, ChangeDetectionStrategy, Component, computed, ElementRef, inject, input, model, Renderer2, signal, viewChild } from '@angular/core';
+import { afterRenderEffect, ChangeDetectionStrategy, Component, computed, ElementRef, inject, input, linkedSignal, model, Renderer2, signal, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import DOMPurify from 'dompurify';
 import * as smd from 'streaming-markdown';
@@ -38,9 +38,18 @@ import { SummarizationService } from '../../ai/services/summarization.service';
 
     parser = signal<smd.Parser | undefined>(undefined);
     error = signal('');
-    chunks = signal('');
-    chunk = signal('');
     isSummarizing = signal(false);
+
+    chunk = signal('');
+    chunks = linkedSignal<string, string>({
+      source: this.chunk,
+      computation: (chunk, previous) => {
+        if (!previous || !previous?.value) {
+          return chunk;
+        }
+        return `${previous.value}${chunk}`;
+      }
+    });
   
     constructor() {
       afterRenderEffect({
@@ -50,9 +59,8 @@ import { SummarizationService } from '../../ai/services/summarization.service';
             console.log('no parser, return');
             return;
           }
-          const chunks = this.chunks();
   
-          DOMPurify.sanitize(chunks);
+          DOMPurify.sanitize(this.chunks());
           if (DOMPurify.removed.length) {
             // If the output was insecure, immediately stop what you were doing.
             // Reset the parser and flush the remaining Markdown.
@@ -70,15 +78,8 @@ import { SummarizationService } from '../../ai/services/summarization.service';
     }
   
     async requestSummary() {
-        const element = this.element();
-        const renderer = smd.default_renderer(element);
-        this.parser.set(smd.parser(renderer));
+        this.clearSummary();
     
-        if (element.lastChild) {
-            console.log('Remove children');
-            this.renderer.setProperty(element, 'innerHTML', '');
-        }
-  
         this.isSummarizing.set(true);
         let summarizer: Summarizer | undefined = undefined;
         try {
@@ -88,19 +89,46 @@ import { SummarizationService } from '../../ai/services/summarization.service';
                     signal: this.summarizationService.signal,
                 });
 
-                for await (const chunk of stream) {
-                    this.chunks.update((prev) => prev + chunk);
-                    this.chunk.set(chunk);
-                }
+                const reader = stream.getReader();
+                this.chunk.set('');            
+                const self = this;
+
+                reader.read()
+                  .then(function processText({ value, done }): any {
+                      if (done) {
+                        return;
+                      }
+                      
+                      self.chunk.set(value);
+                      return reader.read().then(processText);
+                  })
+                  .catch((err) => {
+                      console.error(err);
+                      if (err instanceof Error) {
+                          throw err;
+                      }
+                      throw new Error('Error in streaming the draft.');
+                  })
+                  .finally(() => {
+                    if (summarizer) {
+                      summarizer.destroy();
+                    }
+                    this.isSummarizing.set(false);
+                  });
             }
         } catch (err) {
             console.error(err);
             this.error.set('Error in streaming the summary.');
-        } finally {
-            if (summarizer) {
-                summarizer.destroy();
-            }
-            this.isSummarizing.set(false);
         }
+    }
+
+    private clearSummary(): void {
+      const element = this.element();
+      if (element.lastChild) {
+        this.renderer.setProperty(element, 'innerHTML', '');
+      }
+
+      const markdown_renderer = smd.default_renderer(element);
+      this.parser.set(smd.parser(markdown_renderer));
     }
 }
